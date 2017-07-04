6d43df393fe907a540356ee0bb91ec680125a5ad
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2017-07-04 23:09:54 +0530

    v1
    version 1

---
+++ ChitChat/RegisterationPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterationPageViewController.swift
+//  ChitChat
+//
+//  Created by PRASHUK AJMERA on 5/10/17.
+//  Copyright © 2017 PRASHUK AJMERA. All rights reserved.
+//
+
+import UIKit
+
+class RegisterationPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func signInBtnTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
