40d1097366310e3a232ea51f360005ea50ab156d
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2021-06-13 00:12:33 -0500

    Integrated Firebase

--- Chit Chat/Chit Chat/Controllers/WelcomeViewController.swift
+++ Chit Chat/Chit Chat/Controllers/WelcomeViewController.swift
@@ -6,16 +6,24 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+//        var charIndex = 0.0
+//        titleLabel.text = ""
+//        let titleText = "⚡️ChitChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+        
+        titleLabel.text = "⚡️ChitChat"
     }
-    
-
 }
